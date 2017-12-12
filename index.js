40ee3788fde41b90e75462736a11a0424736efc0
小檀 <474481263@qq.com>
2017-12-12 14:05:30 +0800

    添加任务调度

--- index.js
+++ index.js
@@ -1,45 +1,26 @@
-import mongoose from 'mongoose'
-import Downloader from './src/Downloader'
-import TextExtract from './src/TextExtract'
-import seedsList from './src/SeedsList'
-import config from './src/config'
-import newsService from './src/storage/services/NewsService'
+import cron from 'cron'
+import spider from './spider'
 
-mongoose.Promise = global.Promise
-mongoose.connect(config.mongodbConfig.url, {
-  useMongoClient: true
-})
-
-let spider = () => {
-  console.time('spider')
-  seedsList.forEach(async element => {
-    let respondData = await new Downloader(
-      `${element.host}${element.seed}`
-    ).downloadHTML()
-    let extractData = new TextExtract(element, respondData).extract()
-    storage(element, extractData)
+/**
+ *  # ┌────────────── second (optional)
+ *  # │ ┌──────────── minute
+ *  # │ │ ┌────────── hour
+ *  # │ │ │ ┌──────── day of month
+ *  # │ │ │ │ ┌────── month
+ *  # │ │ │ │ │ ┌──── day of week
+ *  # │ │ │ │ │ │
+ *  # │ │ │ │ │ │
+ *  # * * * * * *
+ */
+function job () {
+  let cronJob = new cron.CronJob({
+    cronTime: '0 30 9 * * *',
+    onTick: () => {
+      spider()
+    },
+    start: false
   })
-  console.timeEnd('spider')
+  cronJob.start()
 }
 
-let storage = async (seedData, newsList) => {
-  let newsListTem = []
-  for (const item of newsList) {
-    let newsData = await newsService.findObj({ url: item.url })
-    if (!newsData && item.url) {
-      newsListTem.push(item)
-    }
-  }
-  console.log(`[${seedData.text}]新增加链接： ${newsListTem.length}`)
-  if (newsListTem.length === 0) {
-    return
-  }
-  try {
-    await newsService.batchSave(newsListTem)
-  } catch (error) {
-    console.log('保存失败')
-    console.log(error)
-  }
-}
-
-spider()
+job()
---
+++ spider.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose'
+import Downloader from './src/Downloader'
+import TextExtract from './src/TextExtract'
+import seedsList from './src/SeedsList'
+import config from './src/config'
+import newsService from './src/storage/services/NewsService'
+
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongodbConfig.url, {
+  useMongoClient: true
+})
+
+let spider = () => {
+  console.time('spider')
+  seedsList.forEach(async element => {
+    let respondData = await new Downloader(
+      `${element.host}${element.seed}`
+    ).downloadHTML()
+    let extractData = new TextExtract(element, respondData).extract()
+    storage(element, extractData)
+  })
+  console.timeEnd('spider')
+}
+
+let storage = async (seedData, newsList) => {
+  let newsListTem = []
+  for (const item of newsList) {
+    let newsData = await newsService.findObj({ url: item.url })
+    if (!newsData && item.url) {
+      newsListTem.push(item)
+    }
+  }
+  console.log(`[${seedData.text}]新增加链接： ${newsListTem.length}`)
+  if (newsListTem.length === 0) {
+    return
+  }
+  try {
+    await newsService.batchSave(newsListTem)
+  } catch (error) {
+    console.log('保存失败')
+    console.log(error)
+  }
+}
+
+export default spider
